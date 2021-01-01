f694c7c5654c2997abd33181c2e0d32057194b7c
zmrbak <20995658@qq.com>
2021-01-01 11:07:10 +0800

    add code

---
+++ 配套代码/EF20/EF20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext=new EF20Entities())
+            {
+                dbContext.Database.Log = info => Console.WriteLine(info);
+                //var result = dbContext.InsertUser("account1", "password1");
+                var result = dbContext.GetAccountByID(2).FirstOrDefault();
+                if(result!=null)
+                {
+                    Console.WriteLine(result.Id + "," + result.Account + "," + result.Password);
+                }
+
+                //foreach (var item in result)
+                //{
+                //    Console.WriteLine(item.Id+","+item.Account+","+item.Password);
+                //}
+
+                
+
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
