ab04fe9ce9761f8943f202af0f8262a77fe81095
zmrbak <20995658@qq.com>
2021-01-17 10:25:13 +0800

    add code

---
+++ 配套代码/EF24/ConsoleApp1/MyEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1
+{
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 配套代码/EF24/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbContext = new Model1())
+            {
+                dbContext.Database.Log = info => Console.WriteLine(info);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    dbContext.MyEntities.Add(new MyEntity { Name = "A" + i });
+                }
+
+                dbContext.SaveChanges();
+
+                var query = from d in dbContext.MyEntities
+                            select d;
+                foreach (var item in query)
+                {
+                    Console.WriteLine("ID:" + item.Id + "\tName:" + item.Name);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
